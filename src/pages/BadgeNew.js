c7625103481ea0b8447aab81fd7a8b13807564df
oddyec <diegomarquez2008@outlook.com>
2021-05-28 00:24:56 -0500

    levantamiento del estado

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import './styles/BadgeNew.css';
-import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar';
-import Badge from '../components/Badge';
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
+  state = { form: {} };
+  handleChange = (e) => {
+    // const nextForm = this.state.form;
+    // nextForm[e.target.name] = e.target.value;
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
   render() {
     return (
       <div>
@@ -18,11 +30,18 @@ class BadgeNew extends React.Component {
           <div className="row">
             <div className="col">
               <Badge
-                firstName="Diego"
-                lastName="Marquez"
-                twitter="diego_marquezEC"
-                jobTitle="Frontend Engineer"
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
                 avatarUrl="https://s.gravatar.com/avatar/6ff72d2b26549147831dae808bf78e81?s=80"
+              />
+            </div>
+            <div className="col">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
               />
             </div>
           </div>
